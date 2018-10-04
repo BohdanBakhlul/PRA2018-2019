79ecdab694ec6556a5b42fa3b8a6a5657aba4487
BohdanBakhlul <pextoshka@gmail.com>
2018-10-04 11:30:12 +0200

    first changes

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,14 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    static Logger log = Logger.getLogger("abc");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        log.info("Hello world");
     }
 }
